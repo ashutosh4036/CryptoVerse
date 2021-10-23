46230adf2a7f434f9780e2cfd0f8ff5deb7f3460
Ashutosh <ashutosh.tripathi_cs19@gla.ac.in>
2021-10-23 11:00:50 +0530

    Layout created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
-
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Router>
+    <App/>
+  </Router>, document.getElementById('root'));
+
